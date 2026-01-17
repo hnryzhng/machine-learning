98e2267ca1ab66d041037ba2e36f9c9058b2f572
Henry Zheng <hnryzhng@gmail.com>
2026-01-16 21:58:02 -0500

    init commit

---
+++ pytorch-fundamentals/init.sh
@@ -0,0 +1,7 @@
+# Run this if executing notebook via local Python runtime instead of Colab
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip3 install torch jupyter torchvision numpy pandas scipy requests matplotlib Pillow
